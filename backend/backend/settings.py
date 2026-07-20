9c0caa028806e174c04e00be65e6ba3a23248177
Raj <dhariwalraj37@gmail.com>
2026-07-20 20:50:36 +0530

    Fix: Unconditionally define STATICFILES_STORAGE to prevent django-cloudinary-storage crash during build

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CORS_ALLOWED_ORIGINS = [
